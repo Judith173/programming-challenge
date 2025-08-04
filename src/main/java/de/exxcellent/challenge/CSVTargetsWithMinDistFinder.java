be148c82100a989f7a937ede2d33f2bb48e4e0bc
Judith173 <postfach2adresse@freenet.de>
2025-08-04 22:06:03 +0200

    refactor: finish refactorization for more abstraction

--- src/main/java/de/exxcellent/challenge/CSVTargetsWithMinDistFinder.java
+++ src/main/java/de/exxcellent/challenge/CSVTargetsWithMinDistFinder.java
@@ -1,39 +1,79 @@
 package de.exxcellent.challenge;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CSVTargetsWithMinDistFinder extends TargetsWithMinDistFinder<String[]>
 {
-    private String fileName;
-    private String targetIdentifier;
-    private String xIdentifier;
-    private String yIdentifier;
+    private final String fileName;
+    private final String targetIdentifier;
+    private final String xIdentifier;
+    private final String yIdentifier;
+    private int targetIndex;
+    private int xIndex;
+    private int yIndex;
 
     public CSVTargetsWithMinDistFinder(String fileName, String targetIdentifier, String xIdentifier, String yIdentifier)
     {
+        //TODO: handle index instead of col_name as identifier
         this.fileName = fileName;
         this.targetIdentifier = targetIdentifier;
         this.xIdentifier = xIdentifier;
         this.yIdentifier = yIdentifier;
+
+        this.targetIndex = -1;
+        this.xIndex = -1;
+        this.yIndex = -1;
     }
 
 
     @Override
-    protected String[][] getData() throws IOException {
-        //return CSVReader.getFileContent(fileName);
-        return new String[0][0];
+    protected List<String[]> getData() throws IOException {
+        List<String[]> content =  CSVReader.getFileContent(fileName);
+        //TODO handle empty list
+        //TODO handdle CSV without header
+        String[] header = content.get(0);
+        updateIndexes(header);
+        content.remove(0);
+
+        return content;
+
     }
     @Override
     protected double getX(String[] row){
-        return 0;
+        //TODO: handle index is -1
+        //TODO: handle parseDouble fail
+        return Double.parseDouble(row[xIndex]);
     }
 
     @Override
     protected double getY(String[] row){
-        return 0;
+        //TODO: handle index is -1
+        //TODO: handle parseDouble fail
+        return Double.parseDouble(row[yIndex]);
     }
     @Override
     protected String getTarget(String[] row){
-        return "";
+        //TODO: handle index is -1
+        return row[targetIndex];
+    }
+
+    private void updateIndexes(String[] header){
+        //TODO: handle identifier is not in header
+        //TODO: don't try to update if identifier is index
+        targetIndex = getIndexOfElement(targetIdentifier, header);
+        xIndex = getIndexOfElement(xIdentifier, header);
+        yIndex = getIndexOfElement(yIdentifier, header);
+    }
+
+    private int getIndexOfElement(String element, String[] a){
+        for (int index = 0; index < a.length; index++)
+        {
+            if (a[index].equals(element)){
+                return index;
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/de/exxcellent/challenge/SmallestTempSpreadDetTest.java
+++ src/test/java/de/exxcellent/challenge/SmallestTempSpreadDetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -14,10 +15,11 @@ public class SmallestTempSpreadDetTest {
 
     @Test
     public void findTargetWithSmallestTempSpreadTest(){
-        SmallestTemperatureSpreadDeterminer stsd = new SmallestTemperatureSpreadDeterminer("weather.csv", "Day", "MnT", "MxT");
+        CSVTargetsWithMinDistFinder ctwmdf = new CSVTargetsWithMinDistFinder("weather.csv", "Day", "MnT", "MxT");
+        //SmallestTemperatureSpreadDeterminer stsd = new SmallestTemperatureSpreadDeterminer("weather.csv", "Day", "MnT", "MxT");
         String expected = "14";
         try{
-            ArrayList<String> smallestTempSpread = stsd.findTargetWithSmallestTempSpread();
+            List<String> smallestTempSpread = ctwmdf.findTargetsWithMinDistance();
             assertEquals(1, smallestTempSpread.size());
             assertEquals(expected, smallestTempSpread.get(0));
             }
