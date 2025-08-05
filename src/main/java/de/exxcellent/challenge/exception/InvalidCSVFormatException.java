9739fe4db0d30eb026ba774c286aef3a75345d8a
Judith173 <postfach2adresse@freenet.de>
2025-08-05 16:16:53 +0200

    docs: document IncalidSCVFormatException

--- src/main/java/de/exxcellent/challenge/exception/InvalidCSVFormatException.java
+++ src/main/java/de/exxcellent/challenge/exception/InvalidCSVFormatException.java
@@ -2,6 +2,9 @@ package de.exxcellent.challenge.exception;
 
 import java.io.IOException;
 
+/**
+ * Should be thrown if a CSV file does not have the required format
+ */
 public class InvalidCSVFormatException extends IOException{
     public InvalidCSVFormatException(String message)
     {
