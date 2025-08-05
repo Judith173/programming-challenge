7486a8ba2751af84e591aa4cfe69250a1e23c142
Judith173 <postfach2adresse@freenet.de>
2025-08-05 10:56:50 +0200

    test: add tests for CSVMinDistTargetsFinder

--- src/test/java/de/exxcellent/challenge/CSVMinDistTargetsFinderTest.java
+++ src/test/java/de/exxcellent/challenge/CSVMinDistTargetsFinderTest.java
@@ -2,6 +2,7 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,19 +12,63 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CSVMinDistTargetsFinderTest {
 
+    private final String dirName = "min-dist-finder-test-files/";
+    private final String defaultFile = "default-csv.csv";
+    private final String defaultTargetCol = "c1";
+    private final String defaultXIdentifier = "c2";
+    private final String defaultYIdentifier = "c3";
+
     @Test
-    public void findTargetsWithMinDist_weather(){
-        CSVMinDistTargetsFinder targetsFinder = new CSVMinDistTargetsFinder("weather.csv", "Day", "MnT", "MxT");
-        String expected_day = "14";
-        try{
-            List<String> daysWithSmallestTempSpread = targetsFinder.findTargetsWithMinDistance();
-            assertEquals(1, daysWithSmallestTempSpread.size());
-            assertEquals(expected_day, daysWithSmallestTempSpread.get(0));
-            }
-        catch (IOException e){
+    public void findTargetsWithMinDist_default(){
+        try {
+            String file = dirName + defaultFile;
+            CSVMinDistTargetsFinder targetsFinder = new CSVMinDistTargetsFinder(file, defaultTargetCol, defaultXIdentifier, defaultYIdentifier);
+            List<String> actual_results = targetsFinder.findTargetsWithMinDistance();
+            assertEquals(1, actual_results.size());
+            assertEquals("b", actual_results.get(0));
+        }
+        catch (IOException e)
+        {
             System.err.println(e.getMessage());
             fail();
+        }
+    }
 
-    }}
+    @Test
+    public void findTargetsWithMinDist_targetIsIdenticalToYIdentifier(){
+        try {
+            String file = dirName + defaultFile;
+            CSVMinDistTargetsFinder targetsFinder = new CSVMinDistTargetsFinder(file, defaultYIdentifier, defaultXIdentifier, defaultYIdentifier);
+            List<String> actual_results = targetsFinder.findTargetsWithMinDistance();
+            assertEquals(1, actual_results.size());
+            assertEquals("2", actual_results.get(0));
+        }
+        catch (IOException e)
+        {
+            System.err.println(e.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    public void findTargetsWithMinDist_XandYIdentifierAreInterchanged(){
+        try {
+            String file = dirName + defaultFile;
+            CSVMinDistTargetsFinder targetsFinder = new CSVMinDistTargetsFinder(file, defaultTargetCol, defaultXIdentifier, defaultYIdentifier);
+            CSVMinDistTargetsFinder targetsFinderInterchanged = new CSVMinDistTargetsFinder(file, defaultTargetCol, defaultYIdentifier, defaultXIdentifier);
+
+            List<String> results = targetsFinder.findTargetsWithMinDistance();
+            List<String> resultsInterchanged = targetsFinderInterchanged.findTargetsWithMinDistance();
+            assertEquals(1, results.size(), resultsInterchanged.size());
+            assertEquals("b", results.get(0), resultsInterchanged.get(0));
+        }
+        catch (IOException e)
+        {
+            System.err.println(e.getMessage());
+            fail();
+        }
+    }
+
+
 
 }
