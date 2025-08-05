51f413db53b4861607cc4216f5d6932f3f62a146
Judith173 <postfach2adresse@freenet.de>
2025-08-05 16:00:15 +0200

    docs: write documentation for Football

--- src/main/java/de/exxcellent/challenge/Football.java
+++ src/main/java/de/exxcellent/challenge/Football.java
@@ -5,12 +5,20 @@ import de.exxcellent.challenge.service.CSVMinDistTargetsFinder;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Identifies the football team(s) with the smallest goal difference (= smallest distance between scored goals and goals
+ * scored against them) by analyzing a csv-file.
+ */
 public class Football {
     private final String csvFileName;
     private final String teamCol;
     private final String goalsCol;
     private final String goalsAllowedCol;
 
+    /**
+     *
+     * @param fileName csv-file with data about the football teams, their scored goals and gaols scored against them
+     */
     public Football(String fileName){
         this.csvFileName = fileName;
         this.teamCol = "Team";
@@ -18,6 +26,13 @@ public class Football {
         this.goalsAllowedCol = "Goals Allowed";
     }
 
+    /**
+     *
+     * @param fileName csv-file with data about the football teams, their scored goals and gaols scored against them
+     * @param teamCol column header of column with information about the team names
+     * @param goalsCol column header of column with scored goals
+     * @param goalsAllowedCol column header of column with information about goals scored against a specific team
+     */
     public Football(String fileName, String teamCol, String goalsCol, String goalsAllowedCol)
     {
         this.csvFileName = fileName;
@@ -26,11 +41,20 @@ public class Football {
         this.goalsAllowedCol = goalsAllowedCol;
     }
 
+    /**
+     * Finds the team(s) with the smallest goal difference
+     * @return list of team names. Has only one entry if there is a unique team with the smallest goal difference
+     * @throws IOException if an error occurs while reading the file or extracting relevant information from it
+     */
     public List<String> findTeamWithSmallestGoalDifference() throws IOException {
         CSVMinDistTargetsFinder teamsFinder = new CSVMinDistTargetsFinder(csvFileName, teamCol, goalsCol, goalsAllowedCol);
         return teamsFinder.findTargetsWithMinDistance();
     }
 
+    /**
+     * Pretty prints the name of the team(s) with the smallest goal difference
+     * @throws IOException if an error occurs while reading the file or extracting relevant information from it
+     */
     public void printTeamsWithSmallestGoalDifference() throws IOException{
         List<String> teamsWithSmallestGoalDifference = findTeamWithSmallestGoalDifference();
         System.out.printf("Team(s) with smallest goal spread       : %s%n", String.join(", ", teamsWithSmallestGoalDifference));
