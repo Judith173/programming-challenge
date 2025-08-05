7f7f1f47498acae94bbb02268f3dc0d2d01c6cb5
Judith173 <postfach2adresse@freenet.de>
2025-08-05 14:35:32 +0200

    style: remove unnecessary import statements

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,9 +1,6 @@
 package de.exxcellent.challenge;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example JUnit 5 test case.
